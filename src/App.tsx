ecd853f888902cda79277189941119966977c880
Max Ono <max@max-ono.com>
2023-04-30 21:03:43 -0400

    chore: Add prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,64 @@
-/* global chrome */ 
+/* global chrome */
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import "./App.css";
+import './App.css';
 
-const getHours = (seconds: number) => Math.floor(seconds / 60 / 60).toString().padStart(2, '0');
-const getMinutes = (seconds: number) => Math.floor(seconds / 60 % 60).toString().padStart(2, '0');
-const getSeconds = (seconds: number) => Math.floor(seconds % 60).toString().padStart(2, '0');
+const getHours = (seconds: number) =>
+  Math.floor(seconds / 60 / 60)
+    .toString()
+    .padStart(2, '0');
+const getMinutes = (seconds: number) =>
+  Math.floor((seconds / 60) % 60)
+    .toString()
+    .padStart(2, '0');
+const getSeconds = (seconds: number) =>
+  Math.floor(seconds % 60)
+    .toString()
+    .padStart(2, '0');
 
 export default function App() {
   const [secondsElapsed, setSecondsElapsed] = useState(0);
 
   useEffect(() => {
     chrome.storage.sync.get(function (result) {
-      const startDate = new Date(result["wt-start"]);
-      const endDate = result["wt-end"] ? new Date(result["wt-end"]) : new Date();
-      
-      setSecondsElapsed(Math.abs(endDate.valueOf() - startDate.valueOf()) / 1000);
+      const startDate = new Date(result['wt-start']);
+      const endDate = result['wt-end']
+        ? new Date(result['wt-end'])
+        : new Date();
+
+      setSecondsElapsed(
+        Math.abs(endDate.valueOf() - startDate.valueOf()) / 1000,
+      );
     });
   }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setSecondsElapsed(previous => previous + 1);
+      setSecondsElapsed((previous) => previous + 1);
     }, 1000);
 
     return () => clearInterval(interval);
   }, [setSecondsElapsed]);
-  
+
   return (
     <div className="App">
       <div className="App__section">
-        <div className="App__tile">
-            {getHours(secondsElapsed)}
-        </div>
+        <div className="App__tile">{getHours(secondsElapsed)}</div>
 
-        <span className="App__label">
-          Hours
-        </span>
+        <span className="App__label">Hours</span>
       </div>
 
       <div className="App__section">
-        <div className="App__tile">
-            {getMinutes(secondsElapsed)}
-        </div>
+        <div className="App__tile">{getMinutes(secondsElapsed)}</div>
 
-        <span className="App__label">
-          Minutes
-        </span>
+        <span className="App__label">Minutes</span>
       </div>
 
       <div className="App__section">
-        <div className="App__tile">
-            {getSeconds(secondsElapsed)}
-        </div>
+        <div className="App__tile">{getSeconds(secondsElapsed)}</div>
 
-        <span className="App__label">
-          Seconds
-        </span>
+        <span className="App__label">Seconds</span>
       </div>
     </div>
   );
